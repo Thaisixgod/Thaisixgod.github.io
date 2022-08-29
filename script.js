2101ac7ba5ba54a237f5762822ff76700a314c38
Thaisigod <t.vicente35.tv@gmail.com>
2022-08-29 11:37:35 -0300

    Created the anchor links for the page

--- script.js
+++ script.js
@@ -1,2 +1,2 @@
 var nome = prompt("Qual seu nome?")
-alert("Ei" + " " + nome + ", " + "estou montando as skills no momento :)")
+alert("Ei" + " " + nome + ", " + "estou configurando os links da página :)")
